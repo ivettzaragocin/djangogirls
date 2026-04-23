9b0a7d3a5ce7da44071b7cd27b9e33a0ae8d59c5
ivettzaragocin <ivett.zaragocin@unl.edu.ec>
2026-04-23 15:03:02 -0500

    Templates modificados para mostrar post desde base de datos.

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,8 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
+    # Aquí obtenemos los posts de la base de datos
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    # Es VITAL que el diccionario {'posts': posts} esté al final
     return render(request, 'blog/post_list.html', {'posts': posts})
